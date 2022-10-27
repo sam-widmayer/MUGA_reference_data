daae3ee17328f40c89ae2acd5a5773f0b7620977
sam-widmayer <samuel.widmayer@jax.org>
2022-10-27 06:43:44 -0400

    change exec permissions

--- code/GigaMUGA_FilePrep.sh
+++ code/GigaMUGA_FilePrep.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#SBATCH -J GM_QC_Test
+#SBATCH -n 1
+#SBATCH -t 5:00:00
+#SBATCH --mem=100M
 
 # Testing GigaMUGA QC Prep Steps
 singularity run docker://sjwidmay/muga_qc:latest code/GigaMUGA_Reference_QC_FilePrep.R
