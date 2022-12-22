0cae7cf03a1f8f034a6f4810d2cb8282ecccee82
Sam Widmayer <samuel.widmayer@jax.org>
2022-12-22 11:12:49 -0500

    Delete GigaMUGA_DOsample_Imputation.sh

--- code/GigaMUGA_DOsample_Imputation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -J GM_DO_IMP
-#SBATCH --mem 500GB
-#SBATCH -t 8:00:00
-
-# Performing Marker QC on GigaMUGA samples
-singularity run docker://sjwidmay/muga_qc:latest code/GigaMUGA_DOsample_Imputation.R
